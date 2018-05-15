fe45fd45e381bd93cf97bb424b7ae54557688e95
eatingfoil@gmail.com <eatingfoil@gmail.com>
2018-05-15 05:41:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,7 +61,8 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return `Sorry, we don't have a credit card on file for you.`;
   } else {
+    finalTotal = total();
     cart = [];
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    return `Your total cost is $${finalTotal}, which will be charged to the card ${cardNumber}.`;
   }
 }
